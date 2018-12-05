f36a55689212accaa19b48735a453cadb94b0ccf
zennenga <astrozees@gmail.com>
2018-12-04 20:19:28 -0800

    fix exchange

--- app/blueprint/gift_exchange.py
+++ app/blueprint/gift_exchange.py
@@ -1,8 +1,10 @@
 from flask import request, render_template
+from flask_login import current_user, login_required
 
 from app.blueprint.base_blueprint import BaseBlueprint
 from app.extensions.db_session import db
 from constant.blueprint_name import BlueprintName
+from exception.auth.not_logged_in import NotLoggedIn
 from model.table.exchange_registration import ExchangeRegistration
 
 
@@ -16,21 +18,32 @@ class GiftExchange(BaseBlueprint):
 
     def build_routes(self):
         @self.route('/', methods=['GET'])
-        def home():
-            # HTML page showing links to:
-            # Summary Blurb
-            # Register (if open)
-            # View santa (if closed)
+        @login_required
+        def exchanges_get():
             return render_template('exchange/home.html')
 
+        @self.route('/details/<id>', methods=['GET'])
+        @login_required
+        def details_get(id):
+            return render_template('exchange/details.html')
+
+        @self.route('/register', methods=['GET'])
+        @login_required
+        def exchange_register_get():
+            return render_template('exchange/register.html')
+
         @self.route('/register', methods=['POST'])
-        def register_post():
-            # TODO some kind of simple auth check
+        def exchange_register_get():
+            if not current_user.is_authenticated:
+                raise NotLoggedIn()
+            exchange_id = request.form['id']
             what_to_get = request.form['what_to_get']
             what_not_to_get = request.form['what_not_to_get']
             who_to_ask_for_help = request.form['who_to_ask_for_help']
 
             registration = ExchangeRegistration(
+                exchange_id=exchange_id,
+                user_id=current_user.id,
                 what_not_to_get=what_not_to_get,
                 what_to_get=what_to_get,
                 who_to_ask_for_help=who_to_ask_for_help
--- constant/exception_subtype.py
+++ constant/exception_subtype.py
@@ -5,3 +5,4 @@ class ExceptionSubtype(Enum):
     UNABLE_TO_AUTHENTICATE = 'unable_to_authenticate'
     FIELD_MISSING = 'field_missing'
     DUPLICATE_EMAIL = 'duplicate_email'
+    NOT_LOGGED_IN = 'not_logged_in'
